69f29b02e1c0bfd3a4477ada0dde6cf290003be9
Ömer Yücel <omeryucel94@gmail.com>
2017-06-10 18:42:49 +0300

    OCR

---
+++ OCR/LicensePlateRecognition/OCR.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace LicensePlateRecognition
+{
+    partial class OCR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumbePlate1 = new System.Windows.Forms.TextBox();
+            this.ptBNumberPlate1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtNumbePlate2 = new System.Windows.Forms.TextBox();
+            this.ptBNumberPlate2 = new System.Windows.Forms.PictureBox();
+            this.txtNumbePlate3 = new System.Windows.Forms.TextBox();
+            this.ptBNumberPlate3 = new System.Windows.Forms.PictureBox();
+            this.txtNumbePlate4 = new System.Windows.Forms.TextBox();
+            this.ptBNumberPlate4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumbePlate1
+            // 
+            this.txtNumbePlate1.BackColor = System.Drawing.Color.Black;
+            this.txtNumbePlate1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtNumbePlate1.ForeColor = System.Drawing.Color.Lime;
+            this.txtNumbePlate1.Location = new System.Drawing.Point(15, 252);
+            this.txtNumbePlate1.Name = "txtNumbePlate1";
+            this.txtNumbePlate1.Size = new System.Drawing.Size(252, 44);
+            this.txtNumbePlate1.TabIndex = 56;
+            this.txtNumbePlate1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ptBNumberPlate1
+            // 
+            this.ptBNumberPlate1.BackColor = System.Drawing.Color.Silver;
+            this.ptBNumberPlate1.Location = new System.Drawing.Point(12, 47);
+            this.ptBNumberPlate1.Name = "ptBNumberPlate1";
+            this.ptBNumberPlate1.Size = new System.Drawing.Size(252, 198);
+            this.ptBNumberPlate1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptBNumberPlate1.TabIndex = 55;
+            this.ptBNumberPlate1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(494, 342);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(95, 37);
+            this.btnLoad.TabIndex = 57;
+            this.btnLoad.Text = "Recognition";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(12, 322);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 60;
+            this.lblCount.Text = "label3";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(370, 322);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(35, 13);
+            this.lblTime.TabIndex = 58;
+            this.lblTime.Text = "label2";
+            // 
+            // txtNumbePlate2
+            // 
+            this.txtNumbePlate2.BackColor = System.Drawing.Color.Black;
+            this.txtNumbePlate2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtNumbePlate2.ForeColor = System.Drawing.Color.Lime;
+            this.txtNumbePlate2.Location = new System.Drawing.Point(284, 252);
+            this.txtNumbePlate2.Name = "txtNumbePlate2";
+            this.txtNumbePlate2.Size = new System.Drawing.Size(252, 44);
+            this.txtNumbePlate2.TabIndex = 61;
+            this.txtNumbePlate2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ptBNumberPlate2
+            // 
+            this.ptBNumberPlate2.BackColor = System.Drawing.Color.Silver;
+            this.ptBNumberPlate2.Location = new System.Drawing.Point(284, 48);
+            this.ptBNumberPlate2.Name = "ptBNumberPlate2";
+            this.ptBNumberPlate2.Size = new System.Drawing.Size(252, 198);
+            this.ptBNumberPlate2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptBNumberPlate2.TabIndex = 60;
+            this.ptBNumberPlate2.TabStop = false;
+            // 
+            // txtNumbePlate3
+            // 
+            this.txtNumbePlate3.BackColor = System.Drawing.Color.Black;
+            this.txtNumbePlate3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtNumbePlate3.ForeColor = System.Drawing.Color.Lime;
+            this.txtNumbePlate3.Location = new System.Drawing.Point(557, 252);
+            this.txtNumbePlate3.Name = "txtNumbePlate3";
+            this.txtNumbePlate3.Size = new System.Drawing.Size(252, 44);
+            this.txtNumbePlate3.TabIndex = 63;
+            this.txtNumbePlate3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ptBNumberPlate3
+            // 
+            this.ptBNumberPlate3.BackColor = System.Drawing.Color.Silver;
+            this.ptBNumberPlate3.Location = new System.Drawing.Point(557, 48);
+            this.ptBNumberPlate3.Name = "ptBNumberPlate3";
+            this.ptBNumberPlate3.Size = new System.Drawing.Size(252, 198);
+            this.ptBNumberPlate3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptBNumberPlate3.TabIndex = 62;
+            this.ptBNumberPlate3.TabStop = false;
+            // 
+            // txtNumbePlate4
+            // 
+            this.txtNumbePlate4.BackColor = System.Drawing.Color.Black;
+            this.txtNumbePlate4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtNumbePlate4.ForeColor = System.Drawing.Color.Lime;
+            this.txtNumbePlate4.Location = new System.Drawing.Point(832, 251);
+            this.txtNumbePlate4.Name = "txtNumbePlate4";
+            this.txtNumbePlate4.Size = new System.Drawing.Size(252, 44);
+            this.txtNumbePlate4.TabIndex = 65;
+            this.txtNumbePlate4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ptBNumberPlate4
+            // 
+            this.ptBNumberPlate4.BackColor = System.Drawing.Color.Silver;
+            this.ptBNumberPlate4.Location = new System.Drawing.Point(832, 47);
+            this.ptBNumberPlate4.Name = "ptBNumberPlate4";
+            this.ptBNumberPlate4.Size = new System.Drawing.Size(252, 198);
+            this.ptBNumberPlate4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptBNumberPlate4.TabIndex = 64;
+            this.ptBNumberPlate4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Root";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Resize";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(554, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Smoothing ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(829, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 69;
+            this.label4.Text = "Color Filtering";
+            // 
+            // OCR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 406);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumbePlate4);
+            this.Controls.Add(this.ptBNumberPlate4);
+            this.Controls.Add(this.txtNumbePlate3);
+            this.Controls.Add(this.ptBNumberPlate3);
+            this.Controls.Add(this.txtNumbePlate2);
+            this.Controls.Add(this.ptBNumberPlate2);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtNumbePlate1);
+            this.Controls.Add(this.ptBNumberPlate1);
+            this.Name = "OCR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OCR";
+            this.Load += new System.EventHandler(this.OCR_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBNumberPlate4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumbePlate1;
+        private System.Windows.Forms.PictureBox ptBNumberPlate1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtNumbePlate2;
+        private System.Windows.Forms.PictureBox ptBNumberPlate2;
+        private System.Windows.Forms.TextBox txtNumbePlate3;
+        private System.Windows.Forms.PictureBox ptBNumberPlate3;
+        private System.Windows.Forms.TextBox txtNumbePlate4;
+        private System.Windows.Forms.PictureBox ptBNumberPlate4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
---
+++ OCR/LicensePlateRecognition/OCR.cs
@@ -0,0 +1,179 @@
+﻿
+using AForge;
+using AForge.Imaging.Filters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using tesseract;
+
+namespace LicensePlateRecognition
+{
+    public partial class OCR : Form
+    {
+
+        #region Initialize
+        private List<string> lstimages = new List<string>(); //Image urlsi için liste oluşturulmaktadır.
+        int position = 0;
+
+        private TesseractProcessor m_tesseract = null;
+        private string m_path = Application.StartupPath + @"\data\";
+        private const string m_lang = "eng";
+        #endregion
+
+        #region InitializeComponent
+        public OCR()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Ocr
+        private string Ocr(Bitmap image) //OCR Metotları
+        {
+            m_tesseract.Clear();
+            m_tesseract.ClearAdaptiveClassifier();
+            
+            return m_tesseract.Apply(image);
+        }
+        #endregion
+
+        #region OCR_Load
+        private void OCR_Load(object sender, EventArgs e)
+        {
+            m_tesseract = new TesseractProcessor();
+            bool succeed = m_tesseract.Init(m_path, m_lang, 3);
+            if (!succeed)
+            {
+                MessageBox.Show("Tesseract initialization failed. The application will exit.");
+                Application.Exit();
+            }
+            m_tesseract.SetVariable("tessedit_char_whitelist", "ABCDEFHKLMNPQRSTUVXYZ-.1234567890").ToString(); //Training Sets //abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ-.1234567890
+            
+            System.Environment.CurrentDirectory = System.IO.Path.GetFullPath(m_path);
+        }
+        #endregion
+
+        #region btnLoad_Click
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lstimages.Clear();
+            timer1.Enabled = false;
+            string folder = Application.StartupPath + "\\Images";
+
+            foreach (string fileName in Directory.GetFiles(folder, "*.png", SearchOption.TopDirectoryOnly))
+            {
+                lstimages.Add(Path.GetFullPath(fileName)); //Image urlsi listeye eklenmektedir.
+            }
+
+            if (lstimages.Count == 0)
+            {
+                foreach (string fileName in Directory.GetFiles(folder, "*.jpg", SearchOption.TopDirectoryOnly))
+                {
+                    lstimages.Add(Path.GetFullPath(fileName)); //Image urlsi listeye eklenmektedir.
+                }
+            }
+            if (lstimages.Count != 0)
+                timer1.Enabled = true;
+        }
+        #endregion
+
+        #region timer1_Tick
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (position < lstimages.Count)
+            {
+                ProcessImage(lstimages[position]); //Listede bulunan ilgili pozisyondaki görüntü ProcessImage metoduna gönderilmetekdir.
+                position++;
+            }
+            else
+                position = 0;
+        }
+        #endregion
+
+        #region ProcessImage
+        public void ProcessImage(string urlImage) //Görüntünün işlendiği metot.
+        {
+            Stopwatch watch = Stopwatch.StartNew(); //Görüntünün işlenmesi sırasında süre tutulması için süre başlatılmaktadır.
+
+            Bitmap root = new Bitmap(urlImage); //Root Image (Default Image)
+            Bitmap bmp = new Bitmap(root); //Resize Image
+            Bitmap bmp1 = new Bitmap(root); //Smoothing Image
+            Bitmap bmp2 = new Bitmap(root); //Color Filtering Image
+            string outputstring = "";
+
+            ///Root
+            outputstring = this.Ocr(root);
+            txtNumbePlate1.Text = outputstring;
+            ptBNumberPlate1.Image = root;
+
+
+            //Resize Image to : 1920x1080
+            outputstring = "";
+            ResizeBilinear filterr = new ResizeBilinear(1920, 1080);
+            bmp = filterr.Apply(bmp);
+
+            outputstring = this.Ocr(bmp);
+            txtNumbePlate2.Text = outputstring;
+            ptBNumberPlate2.Image = bmp;
+
+
+            //Smoothing
+            outputstring = "";
+            if (BitmapFilter.GaussianBlur(bmp1, 4))
+                if (BitmapFilter.GaussianBlur(bmp1, 4))
+                    if (BitmapFilter.MeanRemoval(bmp1, 9))
+                    { }
+            outputstring = this.Ocr(bmp1);
+            txtNumbePlate3.Text = outputstring;
+            ptBNumberPlate3.Image = bmp1;
+
+
+            //Color Filtering
+            outputstring = "";
+            ColorFiltering filter = new ColorFiltering();
+            filter.Green = new IntRange(175, 255);
+            bmp2 = filter.Apply(bmp2);
+
+            outputstring = this.Ocr(bmp2);
+            txtNumbePlate4.Text = outputstring;
+            ptBNumberPlate4.Image = bmp2;
+
+            watch.Stop(); //Stop The Timer
+            lblTime.Text = String.Format("Total Time : {0} Seconds", watch.Elapsed.TotalSeconds);
+            lblCount.Text = position.ToString() + "/" + lstimages.Count.ToString();
+            System.Threading.Thread.Sleep(5000);
+        }
+        #endregion
+
+        #region FilterLicensePlate
+        /*private string FilterLicensePlate(string licensePlate)
+        {
+            licensePlate = licensePlate.Replace(".", "");
+            licensePlate = licensePlate.Replace("-", "");
+
+            licensePlate = licensePlate.Replace(" ", "");
+            licensePlate = licensePlate.Replace("?", "");
+            licensePlate = licensePlate.Replace("_", "");
+            licensePlate = licensePlate.Replace(",", "");
+            licensePlate = licensePlate.Replace("|", "");
+            licensePlate = licensePlate.Replace(" ", "");
+            licensePlate = licensePlate.Replace("l", "");
+            licensePlate = licensePlate.Replace("~", "");
+            licensePlate = licensePlate.Replace(",", "");
+            licensePlate = licensePlate.Replace(":", "");
+            licensePlate = licensePlate.Replace("'", "");
+            licensePlate = licensePlate.Replace("!", "");
+
+            licensePlate = licensePlate.ToUpper();
+            return licensePlate;
+        }*/
+        #endregion
+    }
+}
